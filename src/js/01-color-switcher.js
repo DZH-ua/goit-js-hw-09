d3ad8a190e9b2a01f3d5e4103af2e0688f1140d0
unknown <dzh.3743@gmail.com>
2022-11-14 14:46:53 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', runColorChange);
+stopBtn.addEventListener('click', stopColorChange);
+
+function runColorChange() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    const newColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = `${newColor}`;
+    console.log('Запустили');
+  }, 1000);
+}
+
+function stopColorChange() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  console.log('Остановили');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
